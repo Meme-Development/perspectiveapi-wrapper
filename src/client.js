552535910f30f935c0d612d0f96857a25abb5a2a
Legend108 <80154701+Legend108@users.noreply.github.com>
2021-10-16 20:23:52 +0530

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,1 +1,39 @@
-// Code stuff here
+const { google } = require("googleapis");
+function Analyze (options = { apiKey }, content, resultFn){
+		const opt = options;
+		const key = opt.apiKey;
+		const url =
+			"https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1";
+		google.discoverAPI(url)
+			.then((client) => {
+				const reqbody = {
+					comment: {
+						text: content,
+					},
+					requestedAttributes: {
+						TOXICITY: {},
+					},
+				};
+
+				client.comments.analyze(
+					{
+						key: key,
+						resource: reqbody,
+					},
+					(err, re) => {
+						if (err) return console.error(err);
+						else
+						try {
+							resultFn(re.data.attributeScores.TOXICITY.summaryScore.value.toFixed(2));
+						} catch(err) {
+							throw new Error("Unable to set data");
+						}
+					}
+				);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}
+
+module.exports = Analyze;
