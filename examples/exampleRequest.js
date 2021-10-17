79c685453608f2e7a05bc9e2a7dd44c81c2befc0
Legend108 <80154701+Legend108@users.noreply.github.com>
2021-10-17 16:47:07 +0530

    Update exampleRequest.js

--- examples/exampleRequest.js
+++ examples/exampleRequest.js
@@ -1,6 +1,6 @@
 // An example request using our package!
 
-const toxic_police = require('./src/PerspectiveClient');
+const toxic_police = require('./src/client');
 
 
 toxic_police({ apiKey: "api key here!", content: "Something" }, (res) => {
