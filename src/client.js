b059956070f523c9c3da271a5da866b0f4254fa7
Legend108 <80154701+Legend108@users.noreply.github.com>
2021-10-17 16:14:06 +0530

    BreakingUpdate: Added many new features, syntax changed

--- src/client.js
+++ src/client.js
@@ -1,40 +1,99 @@
 const { google } = require("googleapis");
 
-function Analyze (options = { apiKey }, content, resultFn){
-		const opt = options;
-		const key = opt.apiKey;
-		const url =
-			"https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1";
-		google.discoverAPI(url)
-			.then((client) => {
-				const reqbody = {
-					comment: {
-						text: content,
-					},
-					requestedAttributes: {
-						TOXICITY: {},
-					},
-				};
+class ErrorConstructor extends Error {
+  constructor(msg) {
+    super(msg);
+    this.name = "PerspectiveWrapperError";
+  }
+}
 
-				client.comments.analyze(
-					{
-						key: key,
-						resource: reqbody,
-					},
-					(err, re) => {
-						if (err) return console.error(err);
-						else
-						try {
-							resultFn(re.data.attributeScores.TOXICITY.summaryScore.value.toFixed(2));
-						} catch(err) {
-							throw new Error("Unable to set data");
-						}
-					}
-				);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
-	}
+class WrapperError extends ErrorConstructor {
+  constructor(msg) {
+    super(msg);
+    this.name = "PerspectiveWrapperError";
+  }
+}
+
+function Analyze(
+  options = {
+    apiKey,
+    content,
+    getOriginalData,
+  },
+  resultFn
+) {
+  const opt = options;
+  const key = opt.apiKey;
+  const content = opt.content;
+  const url =
+    "https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1";
+
+  if (typeof resultFn !== "function") {
+    throw new WrapperError(
+      "The typeof resultFn should be a function, you provided " +
+        typeof resultFn
+    );
+  } else if (typeof key !== "string") {
+    throw new WrapperError(
+      "The typeof apiKey should be a string, you provided " + typeof key
+    );
+  } else if (typeof content !== "string") {
+    throw new WrapperError(
+      "The typeof content has to be a string, you provided " + typeof content
+    );
+  }
+
+  if (key.length <= 0) {
+    throw new WrapperError("API key has to be greater than 0 characters");
+  } else if (content.length <= 0) {
+    throw new WrapperError(
+      "The length of the content has to be greater than 0"
+    );
+  }
+
+  google
+    .discoverAPI(url)
+    .then((client) => {
+      const r = {
+        comment: {
+          text: content,
+        },
+        requestedAttributes: {
+          TOXICITY: {},
+        },
+      };
+
+      client.comments.analyze(
+        {
+          key: key,
+          resource: r,
+        },
+        (err, re) => {
+          if (err) throw new WrapperError("Unable to the make request");
+          else
+            try {
+              if (opt.getOriginalData) {
+                resultFn(re.data);
+              } else if (opt.getOriginalValue) {
+                resultFn(re.data.attributeScores.TOXICITY.summaryScore.value);
+              } else if (opt.getOriginalData && opt.getOriginalValue) {
+                throw new WrapperError(
+                  "getOriginalValue and getOriginalData cannot be enable togehter!"
+                );
+              } else if (!opt.getOriginalData && !opt.getOriginalValue) {
+                resultFn(
+                  re.data.attributeScores.TOXICITY.summaryScore.value.toFixed(2)
+                );
+              }
+            } catch (err) {
+              throw new WrapperError(err);
+            }
+        }
+      );
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
 
 module.exports = Analyze;
