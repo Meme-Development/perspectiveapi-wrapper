60f720126268a39b8aba8f3ac231a5d413d69275
Legend108 <80154701+Legend108@users.noreply.github.com>
2021-10-19 12:23:17 +0530

    Another syntax change

--- src/client.js
+++ src/client.js
@@ -1,103 +1,102 @@
 const { google } = require("googleapis");
 
 class ErrorConstructor extends Error {
-  constructor(msg) {
-    super(msg);
-    this.name = "PerspectiveWrapperError";
-  }
+	constructor(msg) {
+		super(msg);
+		this.name = "PerspectiveWrapperError";
+	}
 }
 
 class WrapperError extends ErrorConstructor {
-  constructor(msg) {
-    super(msg);
-    this.name = "PerspectiveWrapperError";
-  }
+	constructor(msg) {
+		super(msg);
+		this.name = "PerspectiveWrapperError";
+	}
 }
+class PerspectiveWrapper {
+	constructor(options={ apiKey: null }) {
+		this.apiKey = options.apiKey ? options.apiKey : null;
+		if (!this.apiKey || this.apiKey.length == 0) {
+			throw new WrapperError("Please provide the API key!");
+		} else if(typeof this.apiKey !== 'string') {
+			throw new WrapperError("The typeof apiKey has to be a string, received " + typeof this.apiKey);
+		}
+	}
 
-function Analyze(
-  options = {
-    apiKey,
-    content,
-    getOriginalData,
-    getOriginalValue,
-  },
-  resultFn
-) {
-  const opt = options;
-  const key = opt.apiKey;
-  const content = opt.content;
-  const url =
-    "https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1";
+	Analyze(
+		content,
+		resultFn
+	) {
+		const key = this.apiKey;
+		const url =
+			"https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1";
 
-  if (typeof resultFn !== "function") {
-    throw new WrapperError(
-      "The typeof resultFn should be a function, you provided " +
-        typeof resultFn
-    );
-  } else if (typeof key !== "string") {
-    throw new WrapperError(
-      "The typeof apiKey should be a string, you provided " + typeof key
-    );
-  } else if (typeof content !== "string") {
-    throw new WrapperError(
-      "The typeof content has to be a string, you provided " + typeof content
-    );
-  }
+		if (typeof resultFn !== "function") {
+			throw new WrapperError(
+				"The typeof resultFn should be a function, you provided " +
+				typeof resultFn
+			);
+		} else if (typeof content !== "string") {
+			throw new WrapperError(
+				"The typeof content has to be a string, you provided " + typeof content
+			);
+		} else if (content.length <= 0 || !content) {
+			throw new WrapperError(
+				"The length of the content has to be greater than 0"
+			);
+		}
 
-  if (key.length <= 0 || !key) {
-    throw new WrapperError(
-      "API key has to be provided / greater than 0 characters"
-    );
-  } else if (content.length <= 0 || !content) {
-    throw new WrapperError(
-      "The length of the content has to be greater than 0"
-    );
-  }
+		google
+			.discoverAPI(url)
+			.then((client) => {
+				const r = {
+					comment: {
+						text: content,
+					},
+					requestedAttributes: {
+						TOXICITY: {},
+					},
+				};
 
-  google
-    .discoverAPI(url)
-    .then((client) => {
-      const r = {
-        comment: {
-          text: content,
-        },
-        requestedAttributes: {
-          TOXICITY: {},
-        },
-      };
+				client.comments.analyze(
+					{
+						key: key,
+						resource: r,
+					},
+					(err, re) => {
+						if (err) throw new WrapperError("Unable to the make request");
+						else
+							try {
+								// if(typeof opt.getOriginalData == 'undefined' && typeof opt.getOriginalValue == 'undefined' || typeof opt.getOriginalData == 'null' && typeof opt.getOriginalValue == 'null') {
+								// 	resultFn(re.data.attributeScores.TOXICITY.summaryScore.value.toFixed(2))
+								// }
+								// if (opt.getOriginalData && opt.getOriginalValue) {
+								// 	throw new WrapperError(
+								// 		"getOriginalValue and getOriginalData cannot be enabled togehter!"
+								// 	);
+								// } else if (!opt.getOriginalData && !opt.getOriginalValue) {
+								// 	resultFn(
+								// 		re.data.attributeScores.TOXICITY.summaryScore.value.toFixed(2)
+								// 	);
+								// } else if (opt.getOriginalData) {
+								// 	resultFn(re.data);
+								// } else if (opt.getOriginalValue) {
+								// 	resultFn(re.data.attributeScores.TOXICITY.summaryScore.value);
+								// }
 
-      client.comments.analyze(
-        {
-          key: key,
-          resource: r,
-        },
-        (err, re) => {
-          if (err) throw new WrapperError("Unable to the make request");
-          else
-            try {
-              if (opt.getOriginalData && opt.getOriginalValue) {
-                throw new WrapperError(
-                  "getOriginalValue and getOriginalData cannot be enabled togehter!"
-                );
-              } else if (!opt.getOriginalData && !opt.getOriginalValue) {
-                resultFn(
-                  re.data.attributeScores.TOXICITY.summaryScore.value.toFixed(2)
-                );
-              } else if (opt.getOriginalData) {
-                resultFn(re.data);
-              } else if (opt.getOriginalValue) {
-                resultFn(re.data.attributeScores.TOXICITY.summaryScore.value);
-              }
-            } catch (err) {
-              throw new WrapperError(err);
-            }
-        }
-      );
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+                 // Fixing this tommorow
+								resultFn(	re.data.attributeScores.TOXICITY.summaryScore.value.toFixed(2)
+								);
+							} catch (err) {
+								throw new WrapperError(err);
+							}
+					}
+				);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}
+
 }
-
-module.exports = Analyze;
-
+module.exports = PerspectiveWrapper;
